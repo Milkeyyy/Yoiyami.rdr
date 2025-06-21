4994c0169832e7abbe2b8d2dd7aff4b25d15e013
Milkeyyy <59532514+Milkeyyy@users.noreply.github.com>
2025-06-21 16:55:44 +0900

    Initial commit

---
+++ Main.cs
@@ -0,0 +1,14 @@
+﻿using Produire;
+
+namespace ProduirePluginTemplate
+{
+	public class SampleClass : IProduireClass
+	{
+		
+	}
+
+	public class SampleStaticClass : IProduireStaticClass
+	{
+		
+	}
+}
